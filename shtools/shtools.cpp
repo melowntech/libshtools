ab5858257036e9394cd0246161ba658c194129d7
Vaclav Blazek <vaclav.blazek@citationtech.net>
2019-07-25 14:52:01 +0200

    added C++ wrapper for MakeGridDH

---
+++ shtools/shtools.cpp
@@ -0,0 +1,145 @@
+/**
+ * Copyright (c) 2019 Melown Technologies SE
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * *  Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * *  Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "dbglog/dbglog.hpp"
+
+#include "utility/streams.hpp"
+
+#include "shtools.hpp"
+
+extern "C" {
+
+void pymakegriddh_(int *exitstatus
+                   , double *griddh, int *n
+                   , double *cilm, const int *lmax
+                   , const int *norm, const int *sampling
+                   , const int *csphase, const int *lmax_calc
+                   , const int *cilm_d0, const int *cilm_d1, const int *cilm_d2
+                   , const int *griddh_d0, const int *griddh_d1);
+
+} // extern "C"
+
+namespace shtools {
+
+namespace {
+
+std::vector<double> prepareCilm(int &lmax, const std::vector<double> &sh)
+{
+    auto tmp(std::sqrt(sh.size()));
+    int size(tmp);
+    if (double(size) != tmp) {
+        LOGTHROW(err2, std::logic_error)
+            << "Number of spherical harmonics must be power of two, not "
+            << sh.size() << ".";
+    }
+
+    if (size == 1) {
+        LOGTHROW(err2, std::logic_error)
+            << "Too little data in harmonics.";
+    }
+
+    // left matrix
+    const auto matrixSize(size * size);
+    std::vector<double> cilm(2 * matrixSize, 0);
+
+    auto ish(sh.begin());
+
+    auto base(cilm.data());
+    for (int j(0); j < size; ++j) {
+        for (int i(0); i <= j; ++i) {
+            base[2 * (i * size + j)] = *ish++;
+        }
+    }
+
+    ++base;
+    for (int j(1); j < size; ++j) {
+        for (int i(1); i <= j; ++i) {
+            base[2 * (i * size + j)] = *ish++;
+        }
+    }
+
+    lmax = size - 1;
+    return cilm;
+}
+
+} // namespace
+
+/** Generates DH longlat grid from spherical harmonics.
+ */
+DHGrid makeGridDH(const std::vector<double> &sh, Sampling sampling
+                  , Norm norm, bool csPhase
+                  , const boost::optional<int> &lmaxCalc)
+{
+    int lmax;
+    auto cilm(prepareCilm(lmax, sh));
+
+    const auto size(lmax + 1);
+    int n(2 * lmax + 2);
+
+    const auto sampling_(static_cast<int>(sampling));
+    const auto norm_(static_cast<int>(norm));
+    const auto csPhase_(csPhase ? -1 : 1);
+    const auto lmaxCalc_(lmaxCalc ? *lmaxCalc : lmax);
+
+    DHGrid grid(sampling_ * n, n);
+
+    const int cilm_d0(2);
+    const int cilm_d1(size);
+    const int cilm_d2(size);
+
+    const int griddh_d0(grid.height());
+    const int griddh_d1(grid.width());
+
+    int exitstatus(-1);
+    pymakegriddh_(&exitstatus
+                  , grid.raw(), &n
+                  , cilm.data(), &lmax
+                  , &norm_, &sampling_, &csPhase_, &lmaxCalc_
+                  , &cilm_d0, &cilm_d1, &cilm_d2, &griddh_d0, &griddh_d1);
+
+    switch (exitstatus) {
+    case 0: break;
+    case 1:
+        LOGTHROW(err2, std::runtime_error)
+            << "makeGridDH: Improper dimensions of input array.";
+    case 2:
+        LOGTHROW(err2, std::runtime_error)
+            << "makeGridDH: Improper bounds for input variable.";
+    case 3:
+        LOGTHROW(err2, std::runtime_error)
+            << "makeGridDH: Error allocating memory.";
+    case 4:
+        LOGTHROW(err2, std::runtime_error)
+            << "makeGridDH: File IO error.";
+    default:
+        LOGTHROW(err2, std::runtime_error)
+            << "makeGridDH: Invalid exit status <" << exitstatus << ">.";
+    }
+
+    return grid;
+}
+
+} // namespace shtools
---
+++ shtools/shtools.hpp
@@ -0,0 +1,83 @@
+/**
+ * Copyright (c) 2019 Melown Technologies SE
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * *  Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * *  Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef shtools_shtools_hpp_included_
+#define shtools_shtools_hpp_included_
+
+#include <vector>
+
+#include <boost/optional.hpp>
+
+#include "math/geometry_core.hpp"
+
+namespace shtools {
+
+/** DH Output grid.
+ */
+class DHGrid {
+public:
+    DHGrid(int width, int height)
+        : size_(width, height), data_(width * height) {}
+
+    double operator()(int x, int y) const {
+        return data_[size_.height * x + y];
+    }
+
+    int width() const { return size_.width; }
+    int height() const { return size_.height; }
+
+    double* raw() { return data_.data(); }
+
+private:
+    math::Size2 size_;
+    std::vector<double> data_;
+};
+
+enum class Norm : int {
+    geodesy = 1
+    , schmidt = 2
+    , unnormalized = 3
+    , orthonormal = 4
+};
+
+enum class Sampling : int {
+    equallySampled = 1
+    , equallySpaced = 2
+};
+
+/** Generates DH grid from sphedical harmonics.
+ *
+ *  \param sh list of parametrs in the form to build both matrices.
+ */
+DHGrid makeGridDH(const std::vector<double> &sh
+                  , Sampling sampling = Sampling::equallySampled
+                  , Norm norm = Norm::geodesy
+                  , bool csPhase = false
+                  , const boost::optional<int> &lmaxCalc = boost::none);
+
+} // namespace shtools
+
+#endif // shtools_shtools_hpp_included_
